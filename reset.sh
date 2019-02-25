f92661295fcfa19b039cade62f0407dfb7b2b4fa
Colby Morrison <colbyamorrison@gmail.com>
2019-02-25 02:20:45 -0500

    wow

--- reset.sh
+++ reset.sh
@@ -3,8 +3,8 @@ ping archlinux.org
 timedatectl set-ntp true
 timedatectl status
 echo Mounting
-mount /dev/sda1 /mnt/boot
-ls /mnt/boot
 mount /dev/sda2 /mnt
 ls /mnt
+mount /dev/sda1 /mnt/boot
+ls /mnt/boot
 ls /mnt/etc/fstab
